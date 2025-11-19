bffb328192045ed2984fe35e588b5738a54a3c95
Nourelhouda Sakli <nourelhouda.sakli@edu.isetcom.tn>
2025-11-20 00:34:03 +0100

    tp3

--- app/src/main/java/com/example/minishopmanager/MainActivity.kt
+++ app/src/main/java/com/example/minishopmanager/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.minishopmanager
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnSuivant = findViewById<Button>(R.id.btnSuivant)
-// Affiche un Toast et lance ProfilActivity
-        btnSuivant.setOnClickListener {
-            Toast.makeText(this, "Bouton cliqué !",
-                Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ProfilActivity::class.java)
+
+        val btnCatalogue = findViewById<Button>(R.id.btncatalog)
+
+        btnCatalogue.setOnClickListener {
+            val intent = Intent(this, CatalogueActivity::class.java)
             startActivity(intent)
         }
     }
